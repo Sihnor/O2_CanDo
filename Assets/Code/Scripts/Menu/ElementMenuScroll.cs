101b53207aa4eef893dbec1bd098171dc3f3f5ae
DESKTOP-UFEEQ7V\pasca <Pascal.keller12@gmail.com>
2024-07-15 13:03:20 +0200

    Changed scroll input type fixed collider input blocking Moved some things in the scene

--- Assets/Code/Scripts/Menu/ElementMenuScroll.cs
+++ Assets/Code/Scripts/Menu/ElementMenuScroll.cs
@@ -1,52 +1,52 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Code.Scripts.Menu
 {
     public class ElementMenuScroll : MonoBehaviour
     {
         [SerializeField] private GameObject ListElements;
-        
+
         [SerializeField] private GameObject MinPos;
         [SerializeField] private GameObject MaxPos;
-        
+
         float MinScroll;
         float MaxScroll;
-        
+
         Vector3 ListOffset;
-        
+
         float GlobalScroll = 0;
-        
+
         private bool bIsInside = false;
-        
+
         public void SetOffset()
         {
             this.MinScroll = this.MinPos.transform.position.y;
             this.MaxScroll = this.MaxPos.transform.position.y;
         }
 
-        private void OnMouseEnter()
-        {
-            this.bIsInside = true;
-        }
-
-        private void OnMouseExit()
-        {
-            this.bIsInside = false;
-        }
-
         private void Update()
         {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit[] hits = Physics.RaycastAll(ray);
+            bIsInside = false;
+            foreach (var hit in hits)
+            {
+                if (hit.collider == GetComponent<Collider>())
+                {
+                    bIsInside = true;
+                }
+            }
+            Debug.Log(bIsInside);
             if (!this.bIsInside) return;
-            
-            float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+            float scroll = Input.mouseScrollDelta.y / 4;
 
             if (scroll == 0) return;
-            
+
             Vector3 position = this.ListElements.transform.position;
-            
+
             position.y -= scroll;
-            
+
             position.y = Mathf.Clamp(position.y, this.MinScroll, this.MaxScroll);
             this.ListElements.transform.position = position;
         }
